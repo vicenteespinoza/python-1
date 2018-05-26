7b4c2f01fb131c861906d692f2e790ff6040adc4
Espinoza <Vicente.Espinoza@adidas-group.com>
2018-05-26 12:50:25 -0400

    primer commit

---
+++ ejemplo.py
@@ -0,0 +1,13 @@
+#! C:\Python27
+# -*- coding: iso-8859-15 -*-
+
+import os,sys
+n1 = int(input("ingrese primer número :"))
+n2 = int(input("ingrese segundo número :"))
+n3 = int(input("ingrese tercer número :"))
+n4 = int(input("ingrese cuarto número :"))
+
+suma = n1+n2+n3+n4
+total = str('el resultado es' )
+
+print(total,suma)
---
+++ suma.py
@@ -0,0 +1,7 @@
+#! C:\Python27
+#-*- coding: iso-8859-15 -*-
+
+import os, sys
+a = int(input("Unidades :"))
+b = int(input("Precio :"))
+print("A pagar=", a*b)
