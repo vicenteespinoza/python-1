792c5974b58477a7e97b61bf6bbdb7849b549cab
vicenteespinoza <vicenteespinoza@gmail.com>
2018-05-28 06:45:44 -0400

    Add files via upload

---
+++ booleanos.py
@@ -0,0 +1,11 @@
+#! C:\Python27
+# -*- coding: iso-8859-15 -*-
+
+M = True
+F = False
+sexo = str (input('indique sexo del bebe M o F = '))
+
+if M == True:
+    print('el sexo del bebe es masculino')
+else:
+    print('el sexo del bebe es femenino')
